8dcd97cc32aeb76ed562c0e1ed67f9ab671bd544
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-07 19:34:26 +0100

    2nd way

--- 2/index.js
+++ 2/index.js
@@ -1,16 +1,32 @@
-// TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
-function Developer(name, tech) {
-  this.name = name;
-  this.tech = tech;
-  this.introduction = () => {
+// // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+
+// // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+
+// // TODO: Create a new object using the 'Developer' constructor
+
+// // TODO: Call the 'introduction()' method on the new object
+// function Developer(name, tech) {
+//   this.name = name;
+//   this.tech = tech;
+//   this.introduction = () => {
+//     console.log(`This is ${this.name} and they like ${this.tech}`);
+//   };
+// }
+
+// const Angela = new Developer("Angela", "HTML");
+// Angela.introduction();
+
+// the below is the same as above
+class Developer {
+  constructor(name, tech) {
+    this.name = name;
+    this.tech = tech;
+  }
+
+  introduction() {
     console.log(`This is ${this.name} and they like ${this.tech}`);
-  };
+  }
 }
 
-const Angela = new Developer("Angela", "HTML");
-Angela.introduction();
-// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
-
-// TODO: Create a new object using the 'Developer' constructor
-
-// TODO: Call the 'introduction()' method on the new object
+const kk = new Developer("kk", "HTML");
+kk.introduction();
