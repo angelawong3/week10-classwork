abd0f4013a195a9ee873cfcafdb76e3be4884bd4
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-07 19:23:50 +0100

    clswk2

---
+++ 2/index.js
@@ -0,0 +1,16 @@
+// TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+function Developer(name, tech) {
+  this.name = name;
+  this.tech = tech;
+  this.introduction = () => {
+    console.log(`This is ${this.name} and they like ${this.tech}`);
+  };
+}
+
+const Angela = new Developer("Angela", "HTML");
+Angela.introduction();
+// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+
+// TODO: Create a new object using the 'Developer' constructor
+
+// TODO: Call the 'introduction()' method on the new object
