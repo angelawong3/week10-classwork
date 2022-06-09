5a510d3f8e5102e5e9c8b97e77a542ff835a9b10
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-09 19:32:46 +0100

    clswk 12

---
+++ 12/algo.test.js
@@ -0,0 +1,27 @@
+const Algo = require("./algo");
+
+describe("Algo", () => {
+  describe("reverse", () => {
+    // TODO: Write a test for the `reverse` method that should take a string as an argument and return a new reversed version of the string
+    const algorithm = new Algo();
+    const result = algorithm.reverse("hello");
+
+    expect(result).toBe("olleh");
+  });
+
+  describe("isPalindrome", () => {
+    // TODO: Write a test for the `isPalindrome` method that should take a string as an argument and return the boolean `true` if the provided string is a palindrome.
+    const algorithm = new Algo();
+    const result = algorithm.isPalindrome("level");
+
+    expect(result).toBe("level");
+  });
+
+  describe("capitalize", () => {
+    // TODO: Write a test for the `capitalize` method that should take a string as an argument and return a new string with the first letter of each word capitalized
+    const algorithm = new Algo();
+    const result = algorithm.capitalize("hello");
+
+    expect(result).toBe("Hello");
+  });
+});
