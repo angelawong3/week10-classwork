3f2210f575946acb8668005bdac755c2535dfd5e
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-09 21:28:27 +0100

    claswk 20

--- 20/test/fileIO.test.js
+++ 20/test/fileIO.test.js
@@ -1,13 +1,19 @@
-const FileIO = require('../fileIO');
+const FileIO = require("../fileIO");
 
-describe('FileIO', () => {
-  describe('read', () => {
+describe("FileIO", () => {
+  describe("read", () => {
     it("should call fs.readFileSync with the passed in 'file' argument", () => {
       // TODO: Your code here
+      const movie = new MovieSearch();
+      const name = "Rocky";
+
+      const url = movie.buildUrl(name);
+
+      expect(url).toEqual(`https://www.omdbapi.com/?t=${name}&apikey=trilogy`);
     });
   });
 
-  describe('write', () => {
+  describe("write", () => {
     it("should call fs.writeFileSync with the passed in 'path' and 'data' arguments", () => {
       // TODO: Your code here
     });
